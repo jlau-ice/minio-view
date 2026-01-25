3482aa29e9d79d281f6838206d07803401ddd833
ice <jlau.ice@gmail.com>
2026-01-25 23:25:05 +0800

    feat(header): 添加自定义Logo组件并优化头部布局
    - 新增Logo组件用于显示MINIO VIEW品牌标识
    - 替换原有的图片logo为新的Logo组件
    - 移除头部容器的内边距以优化布局
    - 减少图标旋转动画的角度从10度到3度
    - 更新样式实现双线文字效果的自定义Logo

---
+++ src/components/logo/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="flex gap-2">
+      <div class="log-mino">MINIO</div>
+      <div class="log-view">VIEW</div>
+    </div>
+    <!-- <div class="text-[10px] pl-[42px] mt-[2px]">在线MINIO查看器</div> -->
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+<style scoped lang="scss">
+.log-mino, .log-view {
+  position: relative;
+  text-align: center;
+  width: 85px;
+  font-weight: bold;
+  font-size: 22px;
+}
+
+.log-mino::before, .log-mino::after,
+.log-view::before, .log-view::after {
+  position: absolute;
+  left: 0;
+  width: 100%;
+  height: 3px;
+  content: "";
+}
+
+.log-mino::before, .log-view::before {
+  top: 0;
+}
+
+.log-mino::after, .log-view::after {
+  bottom: 0;
+}
+
+.log-mino {
+  &::before, &::after {
+    background: #000000;
+  }
+}
+
+.log-view {
+  background: #29b6f6;
+  color: #ffffff;
+
+  &::before, &::after {
+    background: #29b6f6;
+  }
+}
+</style>
